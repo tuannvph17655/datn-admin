f352f9975c15771d249d72ac3c262dce19e70d99
nguyenvantuanDEV <110893485+tuan2910@users.noreply.github.com>
2022-11-10 10:45:22 +0700

    done list material

--- src/app/services/material.service.ts
+++ src/app/services/material.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+let auth_token = window.localStorage.getItem('auth-token');
+
+const headers = new HttpHeaders({
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${auth_token}`
+});
+const requestOptions = {headers : headers}
+const AUTH_API = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaterialService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getListMaterial(materialReq : any){
+    return this.http.post(AUTH_API +`admin/material/search`,materialReq,requestOptions);
+  }
 }
